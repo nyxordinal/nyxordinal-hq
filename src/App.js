3873982d1cace59d0cab44dd6aeac7ca0c9f748d
Nyxordinal <nyxordinal@gmail.com>
2021-10-21 00:46:23 +0700

    Add 404 page

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import LayoutDefault from './layouts/LayoutDefault';
 
 // Views 
 import Home from './views/Home';
+import PageNotFound from './views/404-page';
 
 // Initialize Google Analytics
 ReactGA.initialize(process.env.REACT_APP_GA_CODE);
@@ -37,6 +38,7 @@ const App = () => {
       children={() => (
         <Switch>
           <AppRoute exact path="/" component={Home} layout={LayoutDefault} />
+          <AppRoute component={PageNotFound} />
         </Switch>
       )} />
   );
